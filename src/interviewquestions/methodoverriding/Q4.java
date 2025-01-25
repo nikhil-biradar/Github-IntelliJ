383d6a9ebe8310a68dc1bfa2f34af30b44f4c034
Nikhil <nikhilbiradar878@gmail.com>
2025-01-25 16:53:23 +0530

    First Commit

--- src/interviewquestions/methodoverriding/Q4.java
+++ src/interviewquestions/methodoverriding/Q4.java
@@ -1,4 +1,27 @@
 package interviewquestions.methodoverriding;
 
 public class Q4 {
+    synchronized void m1() {
+        System.out.println("m1-A");
+    }
+
+    void m2(char c) {
+        System.out.println("m2-A");
+    }
+}
+
+class D extends Q4 {
+    @Override
+    public final void m1() {
+        System.out.println("m1-B");
+    }
+    @Override
+    synchronized void m2(char c) {
+        System.out.println("m2-B");
+    }
+    public static void main(String[] args) {
+        Q4 a = new D();
+        a.m1();
+        a.m2('a');
+    }
 }
