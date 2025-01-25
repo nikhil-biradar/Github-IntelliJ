b06c2ed71377304216ff9e72ca823dae6704adc8
Nikhil <nikhilbiradar878@gmail.com>
2025-01-25 15:33:19 +0530

    First Commit

---
+++ src/interviewquestions/methodoverriding/Q1.java
@@ -0,0 +1,17 @@
+package interviewquestions.methodoverriding;
+
+public class Q1 {
+    private void m1() {
+        System.out.println("m1-A");
+    }
+}
+class B extends Q1 {
+//    @Override
+    private void m1() {
+        System.out.println("m1-B");
+    }
+    public static void main(String[] args) {
+        B b = new B();
+        b.m1();
+    }
+}
