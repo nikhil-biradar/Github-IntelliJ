0110b8b6ed87d036584a83b76be69d2fa5d0107d
Nikhil <nikhilbiradar878@gmail.com>
2025-01-25 18:14:36 +0530

    First Commit

--- src/interviewquestions/methodoverriding/Q5.java
+++ src/interviewquestions/methodoverriding/Q5.java
@@ -1,4 +1,23 @@
 package interviewquestions.methodoverriding;
 
+class X {
+    strictfp void method(int a) {
+        System.out.println("One");
+    }
+    strictfp void method(double b) {
+        System.out.println("Two");
+    }
+}
+
+class Y extends X {
+    @Override
+    void method(double b) {
+        System.out.println("Three");
+    }
+}
+
 public class Q5 {
+    public static void main(String[] args) {
+        new Y().method(20);
+    }
 }
