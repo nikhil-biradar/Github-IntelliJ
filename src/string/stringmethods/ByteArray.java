22441596fbe585f9eda0101b8caec36078202309
Nikhil <nikhilbiradar878@gmail.com>
2025-02-02 15:06:15 +0530

    First Commit

---
+++ src/string/stringmethods/ByteArray.java
@@ -0,0 +1,10 @@
+package string.stringmethods;
+
+public class ByteArray {
+    public static void main(String[] args) {
+
+        byte [] b = {65, 66, 67};
+        String st = new String(b);
+        System.out.println(st);
+    }
+}
